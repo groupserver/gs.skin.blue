20da8564b0a3b2ac625d810618d57c5f01f0ad83
Michael JasonSmith <mpj17@onlinegroups.net>
2013-07-31 15:44:45 -0700

    PEP-8

--- gs/skin/blue/interfaces.py
+++ gs/skin/blue/interfaces.py
@@ -1,12 +1,14 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 from zope.publisher.interfaces.browser import IBrowserRequest
 from zope.app.rotterdam import Rotterdam
 
-# Layers just inherit from IBrowserRequest.
+
 class IGSBlueLayer(IBrowserRequest):
     '''GroupServer Blue Layer'''
-# Skins inherit from a bunch of layers. In this case the blue layer
-# and the core Zope Rotterdam layer.
+    # Layers just inherit from IBrowserRequest.
+
+
 class IGSBlueSkin(IGSBlueLayer, Rotterdam):
     '''GroupServer Blue Skin'''
-
+    # Skins inherit from a bunch of layers. In this case the blue layer
+    # and the core Zope Rotterdam layer.
