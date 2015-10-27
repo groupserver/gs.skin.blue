0d28c30d6bc5d9fc12c223470bb3d7b3edbd2d33
Michael JasonSmith <mpj17@onlinegroups.net>
2015-10-27 12:40:34 -0700

    Updating the meatadata

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ import os
 from setuptools import setup, find_packages
 from version import get_version
 
+name = 'gs.skin.blue'
 version = get_version()
 
 with codecs.open('README.rst', encoding='utf-8') as f:
@@ -28,7 +29,7 @@ with codecs.open(os.path.join("docs", "HISTORY.rst"),
     long_description += '\n' + f.read()
 
 setup(
-    name='gs.skin.blue',
+    name=name,
     version=version,
     description="Blue GroupServer skin.",
     long_description=long_description,
@@ -47,10 +48,11 @@ setup(
     keywords='skin, groupserver, blue, css',
     author='Michael JasonSmith',
     author_email='mpj17@onlinegroups.net',
-    url='https://github.com/groupserver/gs.skin.blue/',
+    url='https://github.com/groupserver/{0}'.format(name),
     license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.skin'],
+    namespace_packages=['.'.join(name.split('.')[:i])
+                        for i in range(1, len(name.split('.')))],
     include_package_data=True,
     zip_safe=True,
     install_requires=[
